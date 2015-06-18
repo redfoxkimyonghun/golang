13965da9273f56aae7a21989aa400adde3e57495
redfoxkimyonghun <kim.yonghun@redfox.co.jp>
2015-06-18 16:25:39 +0900

    update

--- training/method/main.go
+++ training/method/main.go
@@ -7,26 +7,32 @@ func main() {
 	g2 := &G{a: 20, b: 20}
 	f1 := F{a: 30, b: 30}
 	f2 := &F{a: 40, b: 40}
-	fmt.Println(g1.sum())
-	fmt.Println(g2.sum())
-	fmt.Println(f1.sum())
-	fmt.Println(f2.sum())
+	g1.calc()
+	g2.calc()
+	f1.calc()
+	f2.calc()
+	fmt.Println(g1.c)
+	fmt.Println(g2.c)
+	fmt.Println(f1.c)
+	fmt.Println(f2.c)
 }
 
 type G struct {
 	a int
 	b int
+	c int
 }
 
-func (g G) sum() int {
-	return g.a + g.b
+func (g G) calc() {
+	g.c = g.a + g.b
 }
 
 type F struct {
 	a int
 	b int
+	c int
 }
 
-func (f *F) sum() int {
-	return f.a + f.b
+func (f *F) calc() {
+	f.c = f.a + f.b
 }
